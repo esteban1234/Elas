e2739ec79c23a06d1cf02f5a55f852e1376ca868
champiiy <albert1047@hotmail.com>
2017-04-02 22:23:02 -0500

    add meta

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,10 +2,9 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>ELAS CONSTRUCTORA DEL MAYAB</title>
-	<!-- <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> -->
+	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
   	<META NAME="Title" content="ELAS CONSTRUCTORA DEL MAYAB en ciudad de mérida.">
   	<META NAME="Author" CONTENT="[]"/>
   	<META name="Description" content="Somos una compañía ubicada en la ciudad de mérida, yucatan, dedicados a ofrecer servicios de alquiler de maquinaria para construcción, minería y actividades forestales, Servicios de construcción de vivienda unifamiliar, Servicios de construcción de inmuebles comerciales, institucionales y de servicios."/>
